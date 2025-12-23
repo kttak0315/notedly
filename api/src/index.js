86105167444e50d53b3e70233085050adda3d4c2
kttak0315 <kttak0315@gmail.com>
2025-12-24 00:11:50 +0900

    Chapter 4.1 completed

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,36 @@
-// index.js
-// This is the main entry point of our application
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!'
+  }
+};
+
+const app = express();
+
+//아폴로 서버 설정
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// 아폴로 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://locallhost:${port}${server.graphqlPath}`
+  )
 );
+
+// app.get('/', (req, res) => res.send('Hello World!!!'));
+
+// app.listen(port, () =>
+//   console.log(`Server running at http://localhost:${port}`)
+// );
